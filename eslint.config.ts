1c523e6fe0cd2e1f5e46ac5d35931626a28709cd
potato6 <batatashow4@gmail.com>
2026-06-24 17:46:58 -0300

    upgrade and fix Eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,15 +1,9 @@
-// @ts-expect-error TS(2792): Cannot find module 'typescript-eslint'. Did you me... Remove this comment to see the full error message
 import tseslint from "typescript-eslint";
-// @ts-expect-error TS(2792): Cannot find module 'eslint-plugin-jsdoc'. Did you ... Remove this comment to see the full error message
 import jsdoc from "eslint-plugin-jsdoc";
-// @ts-expect-error TS(2792): Cannot find module 'eslint-plugin-jest'. Did you m... Remove this comment to see the full error message
 import jest from "eslint-plugin-jest";
-// @ts-expect-error TS(2792): Cannot find module 'eslint-plugin-playwright'. Did... Remove this comment to see the full error message
 import playwright from "eslint-plugin-playwright";
-// @ts-expect-error TS(2792): Cannot find module 'globals'. Did you mean to set ... Remove this comment to see the full error message
 import globals from "globals";
-// @ts-expect-error TS(2792): Cannot find module 'eslint-plugin-prettier'. Did y... Remove this comment to see the full error message
-import eslintConfigPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const logicalRules = {
     "no-cond-assign": "error",
@@ -101,8 +95,7 @@ export default [
             "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
             "no-undef": "off",
             ...logicalRules,
-            // Removed stylisticRules mapping here
-        },
+       },
     },
 
     {
@@ -128,8 +121,7 @@ export default [
                 ePub: "readonly",
                 pdfjsLib: "readonly",
                 toastr: "readonly",
-                SillyTavern: "readonly",
-            },
+           },
         },
     },
 
@@ -140,7 +132,6 @@ export default [
             globals: {
                 ...globals.node,
                 ...globals.jest,
-                SillyTavern: "readonly",
             },
         },
         rules: {
